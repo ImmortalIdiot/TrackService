1a646a14e0770bac1c6ddfb6078131d69e109d84
ImmortalIdiot <max.krasulya@yandex.ru>
2025-11-14 12:51:45 +0300

    build: update dependency management; add starter web dependency

--- audit-service/build.gradle.kts
+++ audit-service/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.immortalidiot.audit"
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
 
     implementation(project(":events-contract"))
