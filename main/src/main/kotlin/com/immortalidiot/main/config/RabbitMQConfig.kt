a1e980457b07f0832f770fbaf4f2b3fc46a28b8b
ImmortalIdiot <max.krasulya@yandex.ru>
2025-11-14 12:49:37 +0300

    refactor: update main configuration

--- main/src/main/kotlin/com/immortalidiot/main/config/RabbitMQConfig.kt
+++ main/src/main/kotlin/com/immortalidiot/main/config/RabbitMQConfig.kt
@@ -1,14 +1,40 @@
 package com.immortalidiot.main.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class RabbitMQConfig {
+
     @Bean
     fun tracksExchange() : TopicExchange {
-        return TopicExchange(EXCHANGE_NAME)
+        return TopicExchange(EXCHANGE_NAME, true, false)
+    }
+
+    @Bean
+    fun messageConverter() : Jackson2JsonMessageConverter {
+        return Jackson2JsonMessageConverter(ObjectMapper().findAndRegisterModules())
+    }
+
+    @Bean
+    fun rabbitTemplate(
+        connectionFactory: ConnectionFactory,
+        messageConverter: Jackson2JsonMessageConverter
+    ): RabbitTemplate {
+        val template = RabbitTemplate(connectionFactory)
+        template.messageConverter = messageConverter
+        template.setConfirmCallback { _, ack, cause ->
+            if (!ack) {
+                println("NACK: Message delivered failed! $cause")
+            }
+        }
+
+        return template
     }
 
     companion object {
