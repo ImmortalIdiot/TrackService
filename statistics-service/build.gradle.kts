f61e0ff9af2b043f0f9ee14e480d06066784a2f5
ImmortalIdiot <max.krasulya@yandex.ru>
2025-11-14 13:23:59 +0300

    feat: create statistics service

---
+++ statistics-service/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.immortalidiot.statistics"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(project(":events-contract"))
+}
+
+
+kotlin {
+    jvmToolchain(21)
+}
